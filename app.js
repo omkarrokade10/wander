1dcbb916d1aebdd9646568690c82bf8ae03ecace
omkarrokade10 <rokadeomkar444@gmail.com>
2024-09-18 19:41:22 +0530

    edit

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const ejsMate=require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js");
 const wrapAsync=require("./utils/wrapAsync.js");
 const session=require("express-session");
+const MongoStore=require("connect-mongo");
 const flash=require("connect-flash");
 const passport=require("passport");
 const LocalStrategy=require("passport-local");
@@ -21,18 +22,19 @@ const listingRouter=require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl=process.env.ATLASDB_URL;
 
 main()
   .then(() => {
     console.log("connected to DB");
   })
   .catch((err) => {
-    console.log('you are encounting an error related to DB');
+    console.log(err);
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -42,20 +44,33 @@ app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
-const sessionOptions={
-  secret:"mysupersecretcode",
-  resave:false,
-  saveUninitialized:true,
-  cookie:{
-    expires:Date.now()+7*24*60*60*1000,
-    maxAge:7*24*60*60*1000,
-    httpOnly:true,
+const store=MongoStore.create({
+  mongoUrl:dbUrl,
+  crypto:{
+    secret:process.env.SECRET,
   },
-};
-
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
+  touchAfter:24*3600,
 });
+store.on("error",()=>{
+  console.log("ERROR IN MONGO SESSION STORE",err);
+});
+
+const sessionOptions={
+  store,
+    secret:process.env.SECRET,
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+      expires:Date.now()+7 * 24 * 60 * 60 * 1000,
+      maxAge:7 * 24 * 60 * 60 * 1000,
+      httpOnly:true,
+    },
+  };
+
+// app.get("/", (req, res) => {
+//   res.send("Hi, I am root");
+// });
+
 
 app.use(session(sessionOptions));
 app.use(flash());
--- init/index.js
+++ init/index.js
@@ -1,12 +1,34 @@
+// const mongoose = require('mongoose');
+// const initData = require("./data.js");
+// const Listing = require('../models/listing');
+
+// const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
+// main().then(()=>{console.log("Connected to DB");}).catch((err)=>{console.log(err)});
+
+// async function main(){
+//     await mongoose.connect(MONGO_URL);
+// };
+
+// const initDB = async ()=>{
+//     await Listing.deleteMany({});
+//     initData.data = initData.data.map((obj) =>({
+//         ...obj,
+//         owner: "66c41668415d4172536bc667"
+//     }));
+//     await Listing.insertMany(initData.data);
+//     console.log("Data was Initialized");
+// }
+// initDB();
+
 const mongoose = require('mongoose');
 const initData = require("./data.js");
 const Listing = require('../models/listing');
 
-const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
+const dbUrl=process.env.ATLASDB_URL;
 main().then(()=>{console.log("Connected to DB");}).catch((err)=>{console.log(err)});
 
 async function main(){
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 };
 
 const initDB = async ()=>{
