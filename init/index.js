5f483209d5489890952c30fc58990dc83a4f9641
omkarrokade10 <rokadeomkar444@gmail.com>
2024-09-19 14:15:07 +0530

    delete comment

--- init/index.js
+++ init/index.js
@@ -1,25 +1,3 @@
-// const mongoose = require('mongoose');
-// const initData = require("./data.js");
-// const Listing = require('../models/listing');
-
-// const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
-// main().then(()=>{console.log("Connected to DB");}).catch((err)=>{console.log(err)});
-
-// async function main(){
-//     await mongoose.connect(MONGO_URL);
-// };
-
-// const initDB = async ()=>{
-//     await Listing.deleteMany({});
-//     initData.data = initData.data.map((obj) =>({
-//         ...obj,
-//         owner: "66c41668415d4172536bc667"
-//     }));
-//     await Listing.insertMany(initData.data);
-//     console.log("Data was Initialized");
-// }
-// initDB();
-
 const mongoose = require('mongoose');
 const initData = require("./data.js");
 const Listing = require('../models/listing');
